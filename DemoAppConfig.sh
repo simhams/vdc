fce90d1ec8dda6af52528ee31c958031ab16ed82
Srini Ganji <srinivasa.r.ganji@avanade.com>
2018-04-10 08:37:49 +0100

    First commit

---
+++ DemoAppConfig.sh
@@ -0,0 +1,18 @@
+# Update system
+sudo apt-get update
+
+# Install node.js & NPM
+#sudo apt-get install npm -y
+#sudo apt-get install nodejs-legacy -y
+
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+
+
+# Clone Git repo for demo app
+sudo apt-get install git -y
+git clone https://github.com/araffe/nodejs-demoapp.git
+cd nodejs-demoapp/
+sudo npm install forever -g
+sudo npm install
+forever start ./bin/www
