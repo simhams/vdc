417e5bdb5006bcec983cd57115e98582f9c154e5
Srini Ganji <srinivasa.r.ganji@avanade.com>
2018-04-10 09:25:27 +0100

    changed template url and updated linux spoke vm config bash script

--- DemoAppConfig.sh
+++ DemoAppConfig.sh
@@ -5,13 +5,18 @@ sudo apt-get update
 #sudo apt-get install npm -y
 #sudo apt-get install nodejs-legacy -y
 
-sudo apt-get install nodejs -y
 sudo apt-get install npm -y
+sudo apt-get install nodejs -y
 
 # Clone Git repo for demo app
 sudo apt-get install git -y
 git clone https://github.com/araffe/nodejs-demoapp.git
+
 cd nodejs-demoapp/
 sudo npm install forever -g
 sudo npm install
+
+# symbolic link
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
 forever start ./bin/www
